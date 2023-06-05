4753e9f573aeb536333282d10d71b3993e183b91
sonchau1997 <chauson23041997@gmail.com>
2023-06-05 13:10:06 +0700

    Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,14 @@
-const PrimaryLayout=()=>{
-    return (
-        <div>primaryLayout</div>
-    )
-}
+
+import Header from 'components/Header';
+import { styled } from 'styled-components';
+
+const StyledLayout = styled.div`
+  background: #caeae6;
+  min-height: 180vh;
+`
+const PrimaryLayout = () => (
+    <StyledLayout>
+        <Header></Header>
+    </StyledLayout>
+);
 export default PrimaryLayout;
